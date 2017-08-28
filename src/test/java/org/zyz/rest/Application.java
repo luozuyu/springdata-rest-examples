c7fb94848d9d8e93684b3f8d3b3425519e87f8f0
luozuyu <luozuyu@alibaba.com>
2017-08-28 16:14:49 +0800

    LN-0001: inital project

--- src/test/java/org/zyz/rest/Application.java
+++ src/test/java/org/zyz/rest/Application.java
@@ -2,17 +2,22 @@ package org.zyz.rest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by fin_tech_2014 on 06/06/2017.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class Application {
 
+    RestTemplate restTemplate = new RestTemplate();
+
     @Test
-    public void userTest() {
+    public void case1() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "luozuyu");
+        map.put("age", 18);
+        restTemplate.postForObject("http://localhost:9090/test", map, String.class);
     }
+
 }
